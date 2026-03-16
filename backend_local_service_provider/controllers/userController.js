2011e37bde476de7271db18d0282c36e9a0e7e7f
asthas0108 <asthas0108@gmail.com>
2026-03-16 18:39:54 +0530

    register and login done

--- backend_local_service_provider/controllers/userController.js
+++ backend_local_service_provider/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 /* CREATE USER */
 export const registerUser = async (req, res) => {
@@ -24,6 +25,42 @@ export const registerUser = async (req, res) => {
     });
 
     res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+/* LOGIN USER */
+export const loginUser = async (req, res) => {
+  try {
+    const { phone, password } = req.body;
+
+    // check user
+    const user = await User.findOne({ phone });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // create token
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    res.json({
+      message: "Login successful",
+      token,
+      user,
+    });
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
